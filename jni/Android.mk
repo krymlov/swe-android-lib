2c2808a74673e54793261f1559756979104920cf
Yuriy Krymlov <yuriy.krymlov@gmail.com>
2021-06-12 21:51:41 +0300

    Version: swe2v10.01

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := swe2v10
+LOCAL_MODULE := swe2v10.01
 
 LOCAL_LDFLAGS   += -ffunction-sections -fdata-sections -Wl,--gc-sections
 LOCAL_CFLAGS    += -ffunction-sections -fdata-sections -fvisibility=hidden -Wall
-LOCAL_SRC_FILES := swecl.c swedate.c swehel.c swehouse.c swejpl.c swemmoon.c swemplan.c sweph.c swephlib.c swejni.c
+LOCAL_SRC_FILES := swedate.c swehouse.c swejpl.c swemmoon.c swemplan.c sweph.c swephlib.c swecl.c swehel.c swejni.c
 include $(BUILD_SHARED_LIBRARY)
