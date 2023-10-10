ed6176ffd8b636c2e9d746b97b56ef8a0a251ce7
Krymlov <krymlov@gmail.com>
2023-10-10 17:02:18 +0300

    ndkVersion "26.0.10792818"

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,6 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := swe-2.10.03j3c
 
 LOCAL_LDFLAGS   += -ffunction-sections -fdata-sections -Wl,--gc-sections
-LOCAL_CFLAGS    += -ffunction-sections -fdata-sections -fvisibility=hidden -Wall
+LOCAL_CFLAGS    += -ffunction-sections -fdata-sections -fvisibility=hidden -Wall -Wno-error=implicit-function-declaration
 LOCAL_SRC_FILES := swedate.c swehouse.c swejpl.c swemmoon.c swemplan.c sweph.c swephlib.c swecl.c swehel.c swejni.c
 include $(BUILD_SHARED_LIBRARY)
