4e2783f20befef7cd3436d6a0f20a36abf06780c
Yuriy <yuriy.krymlov@gmail.com>
2024-02-22 20:08:17 +0200

    2.10.03j3g

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := swe-2.10.03j3c
+LOCAL_MODULE := swe-2.10.03j3d
 
 LOCAL_LDFLAGS   += -ffunction-sections -fdata-sections -Wl,--gc-sections
 LOCAL_CFLAGS    += -ffunction-sections -fdata-sections -fvisibility=hidden -Wall -Wno-error=implicit-function-declaration
