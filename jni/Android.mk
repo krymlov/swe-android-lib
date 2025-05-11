8c96d68d31d70c2201e71876577393c52a1428f6
Krymlov <krymlov@gmail.com>
2025-05-11 20:20:07 +0300

    max-page-size=16384

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := swe-2.10.03j3d
 
-LOCAL_LDFLAGS   += -ffunction-sections -fdata-sections -Wl,--gc-sections
+LOCAL_LDFLAGS   += -ffunction-sections -fdata-sections -Wl,-z,max-page-size=16384,--gc-sections
 LOCAL_CFLAGS    += -ffunction-sections -fdata-sections -fvisibility=hidden -Wall -Wno-error=implicit-function-declaration
 LOCAL_SRC_FILES := swedate.c swehouse.c swejpl.c swemmoon.c swemplan.c sweph.c swephlib.c swecl.c swehel.c swejni.c
 include $(BUILD_SHARED_LIBRARY)
